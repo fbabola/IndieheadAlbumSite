f951c66358ec90f44b739220264f4bc903e1fd62
fbabola <39572432+fbabola@users.noreply.github.com>
2018-11-15 20:07:12 -0500

    Add project files.

---
+++ MusicAlbumSite/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MusicAlbumSite.Startup))]
+namespace MusicAlbumSite
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
