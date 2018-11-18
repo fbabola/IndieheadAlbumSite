8193bcdf394815a39a6c0b0b2ac3d760fb47f993
fbabola <39572432+fbabola@users.noreply.github.com>
2018-11-18 17:38:55 -0500

    Add site base code

--- MusicAlbumSite/App_Start/BundleConfig.cs
+++ MusicAlbumSite/App_Start/BundleConfig.cs
@@ -23,7 +23,7 @@ namespace MusicAlbumSite
                       "~/Scripts/bootstrap.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/bootstrap-slate.min.css",
                       "~/Content/site.css"));
         }
     }
---
+++ MusicAlbumSite/Controllers/AlbumsController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MusicAlbumSite.Models;
+
+namespace MusicAlbumSite.Controllers
+{
+    public class AlbumsController : Controller
+    {
+        // GET: Albums
+        public ActionResult Index(int? pageIndex, string sortBy)
+        {
+            if (!pageIndex.HasValue)
+            {
+                pageIndex = 1;
+            };
+            if (String.IsNullOrWhiteSpace(sortBy))
+            {
+                sortBy = "Name";
+            };
+
+            return Content(String.Format($"pageIndex={pageIndex}&sortBy={sortBy}"));
+        }
+
+        public ViewResult Random()
+        {
+            var album = new Album()
+            {
+                Title = "The Wall",
+                Artist = "Pink Floyd"
+            };
+            return View(album);
+        }
+
+        [Route("movies/releasedIn/{year}/{month:regex(\\d{2}):range(1,12)}")]
+        public ActionResult ByReleaseDate(int year, int month)
+        {
+            return Content($"{year}/{month}");
+        }
+
+        public ActionResult Edit(int id)
+        {
+            return Content("id= " + id);
+        }
+    }
+}
